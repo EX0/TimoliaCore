367fa7e15da41ebb9a816d99bb69b709a38ff6be
EX0 <dariusmewes@me.com>
2013-04-12 16:55:20 +0300

    made /st console compatible

--- src/de/timolia/core/cmds/st.java
+++ src/de/timolia/core/cmds/st.java
@@ -15,19 +15,18 @@ public class st extends TCommand {
 
 	public st(String name) {
 		super(name);
-		setIngame();
 		setMinArgs(1);
 		setUsage("/st <msg>");
 		setDesc("Send Messages to other Staffs");
 	}
 
 	public void perform(CommandSender sender, String[] args) {
-		Player p = (Player) sender;
+		String name = (sender instanceof Player) ? ((Player) sender).getDisplayName() : sender.getName();
 		String msg = "";
 		for (int i = 0; i < args.length; i++)
 			msg += args[i] + " ";
 
-		Message.certain(ChatColor.GOLD + "St - " + ChatColor.GRAY + p.getDisplayName() + ": " + ChatColor.WHITE + ChatColor.translateAlternateColorCodes('&', msg), "tcore.st");
+		Message.certain(ChatColor.GOLD + "St - " + ChatColor.GRAY + name + ": " + ChatColor.WHITE + ChatColor.translateAlternateColorCodes('&', msg), "tcore.st");
 	}
 
 }
